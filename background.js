dfe3635a3ea0867587e9b1a2617514469dad83a7
Miku <masami45@tuta.io>
2020-11-27 17:40:39 +0700

    adding alert not correct url condition

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 const BaseURL = "https://nhentai.net/g";
 
 chrome.browserAction.onClicked.addListener(function(tab) {
-    if (!tab.url.includes(BaseURL)) return undefined;
+    if (!tab.url.includes(BaseURL)) return alert("nhdl-ext only work with url https://nhentai.net/g/*");
     let id = tab.url.slice(`${BaseURL}/`.length).replace(/\//g, "");
 
-    window.open(`https://nhdl2.herokuapp.com/download/nhentai/${id}/?e=cbz`);
+    window.open(`https://nhdl2.herokuapp.com/download/nhentai/${id}`);
 });
