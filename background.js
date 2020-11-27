a3de3d9844dea51dd259115a526be84be8cb6dab
Miku <masami45@tuta.io>
2020-11-27 14:15:36 +0700

    fix nhentai link

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
-const BaseURL = "https://nhentai.net";
+const BaseURL = "https://nhentai.net/g";
 
 chrome.browserAction.onClicked.addListener(function(tab) {
     if (!tab.url.includes(BaseURL)) return undefined;
-    let id = tab.url.slice(`${BaseURL}/g/`.length).replace(/\//g, "");
+    let id = tab.url.slice(`${BaseURL}/`.length).replace(/\//g, "");
 
-    window.open(`https://nhdl2.herokuapp.com/download/nhentai/${id}`);
+    window.open(`https://nhdl2.herokuapp.com/download/nhentai/${id}/?e=cbz`);
 });
