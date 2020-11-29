a76fab81296da7c8826adf9ac1e764f0dd52d19a
Miku <masami45@tuta.io>
2020-11-29 12:51:36 +0700

    replace return not work url with inject script

--- background.js
+++ background.js
@@ -1,8 +1,16 @@
 const BaseURL = "https://nhentai.net/g";
 
-chrome.browserAction.onClicked.addListener(function(tab) {
-    if (!tab.url.includes(BaseURL)) return alert("nhdl-ext only work with url https://nhentai.net/g/*");
-    let id = tab.url.slice(`${BaseURL}/`.length).replace(/\//g, "");
+chrome.browserAction.onClicked.addListener(function (tab) {
+  // return undefined if url is chrome:// url
+  if (tab.url.startsWith("chrome://")) return undefined;
 
-    window.open(`https://nhdl2.herokuapp.com/download/nhentai/${id}`);
+  // return alert if the url not nhentai gallery url
+  if (!tab.url.includes(BaseURL))
+    return chrome.tabs.executeScript({
+      code: `alert("nhdl-ext only work with url https://nhentai.net/g/*")`,
+    });
+    // return alert("nhdl-ext only work with url https://nhentai.net/g/*");
+
+  let id = tab.url.slice(`${BaseURL}/`.length).replace(/\//g, "");
+  window.open(`https://nhdl2.herokuapp.com/download/nhentai/${id}/?e=cbz`);
 });
